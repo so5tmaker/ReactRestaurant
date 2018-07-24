977f1db6aff45437fb890aca735f7b5a6db741cd
Viktor Sivulskiy <svicvg@gmail.com>
2018-07-24 08:27:06 +0600

    The DISHES Array Was Imported Into App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
-import './App.css';
-
 import Menu from './components/MenuComponent';
+import './App.css';
+import { DISHES } from './shared/dishes';
 
 class App extends Component {
+
   render() {
     return (
       <div>
