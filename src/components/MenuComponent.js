8f599cb7dd19839a673847124b6756920df94952
Viktor Sivulskiy <svicvg@gmail.com>
2018-07-31 10:33:41 +0600

    Menu Component Was Turned Into A Function Component

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -3,7 +3,7 @@ import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
 function RenderMenuItem({ dish, onClick }) {
     return (
-        <Card onClick={() => this.props.onClick(dish.id)}>
+        <Card onClick={() => onClick(dish.id)}>
             <CardImg width="100%" src={dish.image} alt={dish.name} />
             <CardImgOverlay>
                 <CardTitle>{dish.name}</CardTitle>
@@ -11,19 +11,22 @@ function RenderMenuItem({ dish, onClick }) {
         </Card>
     );
 }
-const menu = this.props.dishes.map((dish) => {
-    return (
-        <div key={dish.id} className="col-12 col-md-5 m-1">
 
+const Menu = (props) => {
+    const menu = props.dishes.map((dish) => {
+        return (
+            <div key={dish.id} className="col-12 col-md-5 m-1">
+                <RenderMenuItem dish={dish} onClick={props.onClick} />
+            </div>
+        );
+    });
+    return (
+        <div className="container">
+            <div className="row">
+                {menu}
+            </div>
         </div>
     );
-});
-return (
-    <div className="container">
-        <div className="row">
-            {menu}
-        </div>
-    </div>
-);
+}
 
 export default Menu;
