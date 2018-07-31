cfcb8a83df720b1f1e79057745f6d02bc357bac3
Viktor Sivulskiy <svicvg@gmail.com>
2018-07-31 10:16:17 +0600

    The Useless Constructor Was Removed

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -3,10 +3,6 @@ import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
 class Menu extends Component {
 
-    constructor(props) {
-        super(props);
-    }
-
     render() {
 
         const menu = this.props.dishes.map((dish) => {
