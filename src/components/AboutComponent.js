c21a4b47a827346a6a169f5fb413996b5341f80f
Viktor Sivulskiy <svicvg@gmail.com>
2018-08-08 07:11:15 +0600

    Assignment 2 Improvements

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -5,16 +5,18 @@ import { Link } from 'react-router-dom';
 function RenderLeader({ leader }) {
     if (leader != null) {
         return (
-            <Media tag="li">
-                <Media left middle>
-                    <Media object src={leader.image} alt={leader.name} />
+            <div key={leader.id} className="col-12 mt-5">
+                <Media tag="li">
+                    <Media left middle>
+                        <Media object src={leader.image} alt={leader.name} />
+                    </Media>
+                    <Media body className="ml-5">
+                        <Media heading>{leader.name}</Media>
+                        <p>{leader.designation}</p>
+                        <p>{leader.description}</p>
+                    </Media>
                 </Media>
-                <Media body className="ml-5">
-                    <Media heading>{leader.name}</Media>
-                    <p>{leader.designation}</p>
-                    {leader.description}
-                </Media>
-            </Media>
+            </div>
         );
     }
 }
@@ -23,9 +25,7 @@ function About(props) {
 
     const leaders = props.leaders.map((leader) => {
         return (
-            <div key={leader.id} className="col-12 mt-5">
-                <RenderLeader leader={leader} />
-            </div>
+            <RenderLeader leader={leader} />
         );
     });
 
@@ -84,7 +84,9 @@ function About(props) {
                     <h2>Corporate Leadership</h2>
                 </div>
                 <div className="col-12">
-                    {leaders}
+                    <Media list>
+                        {leaders}
+                    </Media>
                 </div>
             </div>
         </div>
