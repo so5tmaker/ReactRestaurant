9aa84edeff6b490819ff8e7e046c894d54c5cf81
Viktor Sivulskiy <svicvg@gmail.com>
2018-08-20 11:04:14 +0600

    Assignment 3 Task 2

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -12,9 +12,11 @@ class CommentForm extends Component {
         this.toggleModal = this.toggleModal.bind(this);
     }
 
-    handleSubmit(values) {
-        console.log("Current state is: " + JSON.stringify(values));
-        alert("Current state is: " + JSON.stringify(values));
+    handleSubmit(event) {
+        this.toggleModal();
+        alert("Rating: " + this.rating.value + " Author: " + this.author.value
+            + " Comment: " + this.comment.value);
+        event.preventDefault();
     }
 
     toggleModal() {
@@ -36,7 +38,34 @@ class CommentForm extends Component {
                 <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
                     <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
                     <ModalBody>
-
+                        <Form onSubmit={this.handleSubmit}>
+                            <FormGroup>
+                                <Label htmlFor='rating'>Rating</Label>
+                                <Input type='select' name='rating' id='rating'
+                                    innerRef={(input) => this.rating = input}>
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </Input>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor='author'>Your Name</Label>
+                                <Input type='text' name='author' id='author'
+                                    placeholder='Your Name'
+                                    innerRef={(input) => this.author = input}
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor='comment'>Comment</Label>
+                                <Input type='textarea' name='comment' id='comment'
+                                    rows="6"
+                                    innerRef={(input) => this.comment = input}
+                                />
+                            </FormGroup>
+                            <Button type='submit' value="submit" className='primary'>Submit</Button>
+                        </Form>
                     </ModalBody>
                 </Modal>
             </div>
