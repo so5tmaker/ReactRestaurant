d9a5a21844de6cd40d4d10a63c313af83867f69f
Viktor Sivulskiy <svicvg@gmail.com>
2018-08-23 08:53:05 +0600

    Some Improvements Was Added To The comments.js File

--- src/redux/comments.js
+++ src/redux/comments.js
@@ -3,7 +3,7 @@ import * as ActionTypes from './ActionTypes';
 
 export const Comments = (state = COMMENTS, action) => {
     switch (action.type) {
-        case action.ADD_COMMENT:
+        case ActionTypes.ADD_COMMENT:
             var comment = action.payload;
             comment.id = state.length;
             comment.date = new Date().toISOString();
